b701892d12b49886c70b4b7a0b6818bf7f19b43f
amitmosk <63066271+amitmosk@users.noreply.github.com>
2021-12-27 00:37:42 +0200

    static methods & vars

--- Client.py
+++ Client.py
@@ -1,133 +1,112 @@
-# An example script to connect to Google using socket
-# programming in Python
-import socket  # for socket
+import socket
 import sys
 from colors import bcolors
 
+RESERVED_PORTS = 120
+TEAM_NAME = "green apes\n"
+UDP_IP = "127.0.0.1"
+UDP_PORT = 13117
+
+def check_offer_message(msg):
+    magic = msg[0:4]
+    msg_type = msg[4]
+    host_port = msg[5:7]
+    if (msg_type != 2):
+        print(f"{bcolors.RED}Illegal message type")
+        return -1
+    if (magic != b"\xab\xcd\xdc\xba"):
+        print(f"{bcolors.RED}Illegal message magic number")
+        return -1
+    ans = int.from_bytes(host_port, "big")
+    if ans > RESERVED_PORTS:
+        return ans
+    else:
+        print(f"{bcolors.RED}Illegal host port")
+        return -1
 
 class Client:
     def __init__(self):
-        self.team_name = "green apes\n"
-        self.RESERVED_PORTS = 120
+        self.TCP_socket = None
         self.start_client()
 
     def start_client(self):
-        ## ------------------ STATE 1 -------------------------------------------------------
+        server_ip, server_port, failed = self.state_1()
+        if failed:
+            return
+        print(f"{bcolors.OKBLUE}Received offer from " + server_ip + ", attempting to connect...")
+        failed = self.state_2(server_ip, server_port)
+        if failed:
+            return
+        self.state_3()
+        try:
+            # closing TCP socket
+            self.TCP_socket.close()
+        except Exception as e:
+            print(f"{bcolors.RED}Failed closing TCP socket")
+            print(e)
+
+    def state_1(self):
+        # -- CREATE UDP SOCKET
         print(f"{bcolors.OKBLUE}Client started, listening for offer requests...{bcolors.RED}")
         try:
             UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        except socket.error as err:
-            print(f"{bcolors.RED}socket creation failed with error %s" % (err))
-            self.start_client()
-            return
-
-        UDP_IP = "127.0.0.1"
-        UDP_PORT = 13117
-
-
-        # UDP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # UDP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        UDP_socket.bind((UDP_IP, UDP_PORT))
-        # receive broadcast via UDP
-        try:
+            UDP_socket.bind((UDP_IP, UDP_PORT))
             offer_message, addr = UDP_socket.recvfrom(1024)
-            server_port = self.check_offer_message(offer_message)
+            server_port = check_offer_message(offer_message)
             server_ip = addr[0]
+            UDP_socket.close()
             if server_port == -1:
-                UDP_socket.close()
-                self.start_client()
-                return
+                print(f"{bcolors.RED}Broken message from server")
+                return server_ip, server_port, True
+            else:
+                return server_ip, server_port, False
         except Exception as err:
+            print(f"{bcolors.RED}UDP socket creation & get broadcast message failed with error %s" % (err))
             UDP_socket.close()
-            print(err)
-            print(f"{bcolors.RED}cant receive from UDP connection")
-            self.start_client()
-            return
+            return None, None, True
 
-        print(f"{bcolors.OKBLUE}Received offer from " + server_ip + ", attempting to connect...")
-        ## ------------------ STATE 2 -------------------------------------------------------
-        # create TCP socket
+    def state_2(self, server_ip, server_port):
         try:
+            # create TCP socket
             TCP_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socket.error as err:
-            print(f"{bcolors.RED}socket creation failed with error %s" % (err))
-            UDP_socket.close()
-            self.start_client()
-            return
-
-        # connect TCP server socket
-        try:
+            # connect TCP server socket
             TCP_socket.connect((server_ip, server_port))
-        except socket.error as e:
-            print(f"{bcolors.RED}cant connect to server")
-            print(e)
-            UDP_socket.close()
+            self.TCP_socket = TCP_socket
+            return False
+        except Exception as err:
+            print(f"{bcolors.RED}TCP socket creation & connect to the server failed with error %s" % (err))
             TCP_socket.close()
-            self.start_client()
+            return True
+
+    def state_3(self):
+        try:
+            # send team name to the server
+            self.TCP_socket.send(TEAM_NAME.encode())
+        except Exception as e:
+            print(f"{bcolors.RED}Failed sending team name message")
+            print(e)
             return
-
-        ## ------------------ STATE 3 -------------------------------------------------------
-
-        # closing UDP connection
-        UDP_socket.close()
-        # send team name to the server
-        send_name_flag = False
-        while not send_name_flag:
-            try:
-                TCP_socket.send(self.team_name.encode())
-                send_name_flag = True
-            except socket.error as e:
-                print(e)
-                print(f"{bcolors.RED}didnt send team name message, try again..")
-
-        # get & print the question
-        get_question_flag = False
-        while not get_question_flag:
-            try:
-                print(TCP_socket.recv(1024).decode())
-                get_question_flag = True
-                our_answer = input(f"{bcolors.PINK}Enter your answer:")
-            except Exception as e:
-                print(e)
-                print(f"{bcolors.RED}didnt got the question message, try again..")
-
-        # send our answer to server
-        send_answer_flag = False
-        while not send_answer_flag:
-            try:
-                TCP_socket.send(our_answer.encode())
-                send_answer_flag = True
-            except Exception as e:
-                print(e)
-                print(f"{bcolors.RED}didnt send the answer message, try again..")
-
-        # get the summary
-        get_summary_flag = False
-        while not get_summary_flag:
-            try:
-                print(f"{bcolors.OKGREEN}--------------")
-                print(TCP_socket.recv(1024).decode())
-                get_summary_flag = True
-            except Exception as e:
-                print(e)
-                print(f"{bcolors.RED}didnt got the summary message, try again..")
-
-        TCP_socket.close()
-
-
-    def check_offer_message(self, msg):
-        magic = msg[0:4]
-        msg_type = msg[4]
-        host_port = msg[5:7]
-        if (msg_type != 2):
-            print(f"{bcolors.RED}Illegal message type")
-            return -1
-        if (magic != b"\xab\xcd\xdc\xba"):
-            print(f"{bcolors.RED}Illegal message magic number")
-            return -1
-        ans = int.from_bytes(host_port, "big")
-        if ans > self.RESERVED_PORTS:
-            return ans
-        else:
-            print(f"{bcolors.RED}Illegal host port")
-            return -1
+        try:
+            # get & print the question
+            print(self.TCP_socket.recv(1024).decode())
+            # getting answer from user
+            our_answer = input(f"{bcolors.PINK}Enter your answer:")
+        except Exception as e:
+            print(f"{bcolors.RED}Failed getting the question message")
+            print(e)
+            return
+        try:
+            # send our answer to the server
+            self.TCP_socket.send(our_answer.encode())
+        except Exception as e:
+            print(f"{bcolors.RED}Failed sending the answer message")
+            print(e)
+            return
+        try:
+            # get the summary from server
+            print(f"{bcolors.OKGREEN}--------------")
+            print(self.TCP_socket.recv(1024).decode())
+        except Exception as e:
+            print(f"{bcolors.RED}Failed getting the summary message")
+            print(e)
+            return
--- Server.py
+++ Server.py
@@ -5,7 +5,6 @@ from socket import *
 import threading
 import time
 
-
 # s.bind(('', port))
 # print("socket binded to %s" % (port))
 # # put the socket into listening mode
@@ -13,106 +12,21 @@ import time
 # print("socket is listening")
 from colors import bcolors
 
-class Server:
-    def __init__(self):
-        self.start_server()
+finish_broadcast_flag = False
+finish_game_flag = False
+winner = ["Nobody, its a Draw!!!"]
+UDP_destination_port = 13117
 
-
-    def start_server(self):
-        # host port - maybe the return value of the TCP Socket    or    BIND
-        our_IP = "127.0.0.1"
-        UDP_destination_port = 13117
-        UDP_addr = (our_IP, UDP_destination_port)
-
-        print(f"{bcolors.OKBLUE}Server started, listening on IP address " + our_IP)
-
-        ## ----------------------------------- STATE 1 - Waiting for clients --------------------------------------------------
-
-
-        TCP_socket = self.create_bind_TCP_socket()
-
-        # get host port from the socket
-        host_port = TCP_socket.getsockname()[1]
-        offer_message = self.build_msg(host_port)
-
-        #create UDP socket
-        UDP_socket = socket(AF_INET, SOCK_DGRAM)
-
-
-        # set UDP socket flags to support multiple clients
-        UDP_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        UDP_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-
-        # set TCP socket to listen to maximum 2 clients
-        TCP_socket.listen(2)
-        send_thread_flag = [False]
-
-        # create thread to manage broadcast sends
-        send_broadcast = Send_UDP_thread(offer_message, UDP_socket, UDP_addr, send_thread_flag)
-        send_broadcast.start()
-
-        # establish connection with client
-
-
-        c1, addr1 = TCP_socket.accept()
-        c2, addr2 = TCP_socket.accept()
-
-        # set flag to kill sending thread
-        send_thread_flag[0] = True
-        print("its changed")
-        send_broadcast.join()
-
-        #wait 10 second before starting the game
-        time.sleep(10)
-
-        #closing UDP socket
-        UDP_socket.close()
-
-
-        ## ------------------------------------------------ STATE 2 - Game Mode ------------------------------------------------------------
-        # receive group names from the clients
-        group_1_name, group_2_name = self.receive_group_names(c1, c2)
-
-        # generate question
-        rand_question, answer = self.get_question()
-
-        math_question_message = "Welcome to Quick Maths.\nPlayer 1:" + group_1_name + " \nPlayer 2: " + group_2_name + "\n == \nPlease answer the following question as fast as you can:\n" + rand_question
-
-        # set flag to stop the game
-        flag = [False]
-        winner = ["Nobody, its a Draw!!!"]
-        t1 = Client_thread(c1, math_question_message, answer, group_1_name, group_2_name, flag, winner)
-        # t2 = Client_thread(c2, math_question_message, answer, group_2_name, group_1_name, flag, winner)
-        t1.start()
-        # t2.start()
-        t1.join()
-        # t2.join()
-        final_message = "Game Over! \nThe correct answer was " + str(answer) + "!\n \nCongratulations to the winner : " + winner[0]
-        # send final message to the clients
-        c1.send(final_message.encode())
-        # c2.send(final_message.encode())
-
-        # close TCP connection
-        c1.close()
-        # c2.close()
-        TCP_socket.close()
-
-        # print game summary
-        print(f"{bcolors.OKBLUE} summary:" + final_message)
-        print(f"{bcolors.OKBLUE}Game Over, sending out offer requests...")
-
-        #start over the game
-        self.start_server()
-
-    def build_msg(self, host_port):
-        # build the message according the format
-        magic_cookie = bytes.fromhex('abcddcba')
-        message_type = bytes.fromhex('02')
-        host_port_bytes = host_port.to_bytes(2, "big")
-        offer_message = b''.join([magic_cookie, message_type, host_port_bytes])
-        return offer_message
-
-    def get_question(self):
+def build_msg(host_port):
+    # build the message according the format
+    magic_cookie = bytes.fromhex('abcddcba')
+    message_type = bytes.fromhex('02')
+    host_port_bytes = host_port.to_bytes(2, "big")
+    offer_message = b''.join([magic_cookie, message_type, host_port_bytes])
+    return offer_message
+def get_question():
+    is_bad_answer = True
+    while is_bad_answer:
         param1 = random.randint(1, 10)
         param2 = random.randint(1, 10)
         sign_rand = random.randint(0, 3)
@@ -137,39 +51,137 @@ class Server:
                 sign = "/"
         if -1 < answer < 10:
             rand_question = "how much is " + str(param1) + sign + str(param2)
-            return rand_question, answer
-            #return param1, param2, sign, answer
-        else:
-            return self.get_question()
+            is_bad_answer = False
+    return rand_question, answer
+
+
+class Server:
+    def __init__(self):
+        self.our_IP = "127.0.0.1"
+
+        self.start_server()
+
+    def start_server(self):
+        # host port
+        UDP_addr = (self.our_IP, UDP_destination_port)
+        print(f"{bcolors.OKBLUE}Server started, listening on IP address " + self.our_IP)
+        # -- STATE 1 - Waiting for clients --------------------------------------------------
+        # -- Create TCP socket
+        TCP_socket, host_port, failed = self.create_bind_TCP_socket()
+        if failed:
+            return
+        # Build offer for clients
+        offer_message = build_msg(host_port)
+        # -- Create UDP socket
+        UDP_socket, failed = self.create_UDP_socket()
+        if failed:
+            return
+        # -- Set TCP socket to listen to maximum 2 clients
+        try:
+            TCP_socket.listen(2)
+        except Exception as e:
+            print(f"{bcolors.RED}Error in TCP socket")
+            print(e)
+            TCP_socket.close()
+            return
+        # -- Create thread to manage broadcast sends
+        send_broadcast = Send_UDP_thread(offer_message, UDP_socket, UDP_addr)
+        send_broadcast.start()
+        # -- Establish connection with client
+        try:
+            c1, addr1 = TCP_socket.accept()
+            c2, addr2 = TCP_socket.accept()
+        except Exception as e:
+            print(f"{bcolors.RED}Error in TCP socket")
+            print(e)
+            # TCP_socket.close()
+            # c1.close()
+            # c2.close()
+            # return
+        # -- Set flag to kill sending thread
+        global finish_broadcast_flag
+        finish_broadcast_flag = True
+        send_broadcast.join()
+        # -- Wait 10 second before starting the game
+        time.sleep(10)
+        # -- STATE 2 - Game Mode ------------------------------------------------------------
+        # receive group names from the clients
+        group_1_name, group_2_name = self.receive_group_names(c1, c2)
+        # generate question
+        rand_question, answer = get_question()
+        math_question_message = "Welcome to Quick Maths.\nPlayer 1:" + group_1_name + " \nPlayer 2: " + "group_2_name" + \
+                                "\n == \nPlease answer the following question as fast as you can:\n" + rand_question
+        # set flag to stop the game
+        t1 = Client_thread(c1, math_question_message, answer, group_1_name, group_2_name)
+        t2 = Client_thread(c2, math_question_message, answer, group_2_name, group_1_name)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+        final_message = "Game Over! \nThe correct answer was " + str(
+            answer) + "!\n \nCongratulations to the winner : " + winner[0]
+        # send final message to the clients
+        try:
+            c1.send(final_message.encode())
+            c2.send(final_message.encode())
+        except Exception as e:
+            print(f"{bcolors.RED}Failed sending final message")
+            print(e)
+
+        # close TCP connection
+        try:
+            c1.close()
+            c2.close()
+            TCP_socket.close()
+        except Exception as e:
+            print(f"{bcolors.RED}Failed closing sockets")
+            print(e)
+
+        # print game summary
+        print(f"{bcolors.OKBLUE} summary:" + final_message)
+        print(f"{bcolors.OKBLUE}Game Over, sending out offer requests...")
+
+    def create_UDP_socket(self):
+        try:
+            UDP_socket = socket(AF_INET, SOCK_DGRAM)
+            # -- Set UDP socket flags to support multiple clients
+            UDP_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+            UDP_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+            return UDP_socket, False
+        except Exception as e:
+            print(f"{bcolors.RED}Failed in open an UDP socket")
+            print(e)
+            UDP_socket.close()
+            return None, True
 
     def create_bind_TCP_socket(self):
-        bind_flag = False
-        while not bind_flag:
-            try:
-                # create TCP socket
-                TCP_socket = socket(AF_INET, SOCK_STREAM)
-                TCP_socket.bind(('', 0))
-            except Exception as e:
-                # should not happen
-                print(e)
-                bind_flag = True
+        try:
+            # create TCP socket
+            TCP_socket = socket(AF_INET, SOCK_STREAM)
+            TCP_socket.bind(('', 0))
+            host_port = TCP_socket.getsockname()[1]
+            return TCP_socket, host_port, False
+        except Exception as e:
+            print(f"{bcolors.RED}Failed in open a TCP socket")
+            print(e)
+            TCP_socket.close()
+            return None, None, True
 
     def receive_group_names(self, c1, c2):
-        recv_flag = False
-        while not recv_flag:
+        receive_flag = False
+        while not receive_flag:
             try:
                 # receive group names from the clients
                 group_1_name = c1.recv(1024).decode()
                 group_2_name = c2.recv(1024).decode()
                 return group_1_name, group_2_name
-                recv_flag = True
             except socket.error as e:
-                print(e)
                 print(f"{bcolors.RED}didnt receive team name message, try again..")
+                print(e)
 
 
 class Client_thread(threading.Thread):
-    def __init__(self, connection, question, answer, myName, opponnetName, flag, winner):
+    def __init__(self, connection, question, answer, myName, opponnetName):
         threading.Thread.__init__(self)
         self.connection = connection
         self.question = question
@@ -177,47 +189,48 @@ class Client_thread(threading.Thread):
         self.myName = myName
         print(myName)
         self.opponnetName = opponnetName
-        self.flag = flag
-        self.winner = winner
 
     def run(self):
-        self.connection.send(self.question.encode())
         try:
-            # send client the message_math_question
+            # send meesage
             self.connection.settimeout(10)
+            self.connection.send(self.question.encode())
             # get answer and check
             client_answer = self.connection.recv(1024).decode()
             self.finish_game(client_answer)
         except Exception as e:
             # draw
-            print(f"{bcolors.YELLOW}PASS")
             pass
 
     def finish_game(self, client_answer):
-        if self.flag == [False]:
+        global finish_game_flag
+        global winner
+        if finish_game_flag is False:
             if int(client_answer) == self.answer:
-                self.winner[0]= self.myName
-                print(f"{bcolors.YELLOW}1111")
+                winner[0] = self.myName
             else:
-                self.winner[0] = self.opponnetName
-                print(f"{bcolors.YELLOW}222222222")
-
-            self.flag = [True]
-
-
+                winner[0] = self.opponnetName
+            finish_game_flag = True
 
 
 class Send_UDP_thread(threading.Thread):
-    def __init__(self, offer_message, UDP_socket, UDP_addr, flag):
+    def __init__(self, offer_message, UDP_socket, UDP_addr):
         threading.Thread.__init__(self)
         self.offer_message = offer_message
         self.UDP_socket = UDP_socket
         self.UDP_addr = UDP_addr
-        self.flag = flag
-
 
     def run(self):
         # send broadcast - every second
-        while self.flag[0] == "amit":
+        global finish_broadcast_flag
+        while finish_broadcast_flag is False:
             time.sleep(1)
-            self.UDP_socket.sendto(self.offer_message,  self.UDP_addr)
+            try:
+                self.UDP_socket.sendto(self.offer_message, self.UDP_addr)
+            except Exception as e:
+                pass
+        # closing UDP socket
+        try:
+            self.UDP_socket.close()
+        except Exception as e:
+            pass
